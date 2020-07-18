5410ef2c694cf2b95bd360bacb158ef8d3527d98
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-07-18 17:43:14 -0400

    working on deploy errors

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgettracker", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
+// console.log("Before html-routes.js")
+// require("./routes/html-routes.js")(app);
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
